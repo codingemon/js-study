edc455baa6709f7e86d2f12ab2dc30ed759e8053
snoopy1224 <misty12024@gmail.com>
2023-05-22 11:52:48 +0900

    dataset.id 테크닉 사용해보기

--- level2/tab.js
+++ level2/tab.js
@@ -13,15 +13,34 @@
 
 // for문법은 코드복붙보다는 반복실행이 맞음(별차이없음)
 // for (let i = 0; i < 5; i++)  내가 5개를 띄우게 했던것 밑은 답
-for (let i = 0; i < $(".tab-button").length; i++) {
-  $(".tab-button")
-    .eq(i)
-    .on("click", function () {
-      $(".tab-button").removeClass("orange");
-      $(".tab-button").eq(i).addClass("orange");
-      $(".tab-content").removeClass("show");
-      $(".tab-content").eq(i).addClass("show");
-    });
+// for (let i = 0; i < $(".tab-button").length; i++) {
+//   $(".tab-button")
+//     .eq(i)
+//     .on("click", function () {
+//       탭열기(i);
+//     });
+// }
+
+$(".list").click(function (e) {
+  // if (e.target == document.querySelectorAll(".tab-button")[0]) {
+  //   탭열기(0);
+  // }  if (e.target == document.querySelectorAll(".tab-button")[1]) {
+  //   탭열기(1);
+  // }  if (e.target == document.querySelectorAll(".tab-button")[2]) {
+  //   탭열기(2);
+  // }
+
+  // 탭열기(지금누른버튼에 숨겨져 있는 Data-id);
+  // 숫자로 변환해서 쓰면 더 정확할듯
+  탭열기(e.target.dataset.id);
+});
+
+// '숫자'를 넣어줘야지 여러가지 숫자를 대입해서 잘 작동합니다.
+function 탭열기(숫자) {
+  $(".tab-button").removeClass("orange");
+  $(".tab-button").eq(숫자).addClass("orange");
+  $(".tab-content").removeClass("show");
+  $(".tab-content").eq(숫자).addClass("show");
 }
 
 // $(".tab-button")
