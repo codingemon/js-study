47348fc82a159e2b4d97ca2294f9298a4879ef32
snoopy1224 <misty12024@gmail.com>
2023-09-20 18:47:47 +0900

    변수 연습문제

--- 2/7/index.js
+++ 2/7/index.js
@@ -1,13 +1,20 @@
 // 문제1
 함수();
 function 함수() {
-  console.log(안녕); //undefined
+  // let, const는 저런 행위 금지
+  // undefined 자동으로 할당 안됨
+  // var: Hoisitng시 undefined 할당 됨
+  // let,const X
+  console.log(안녕); //error
   let 안녕 = "Hello!";
 }
 
 // 문제2
+함수(); // error
+// function 함수(){} : 전부가 Hoisting 됨
+// var 함수 = function(){}: 선언 부분만 Hositing
 var 함수 = function () {
-  console.log(안녕); // Hello
+  console.log(안녕);
   var 안녕 = "Hello!";
 };
 
