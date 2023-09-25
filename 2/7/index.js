34451a0b4b420ef48b51d9c75744190358149395
snoopy1224 <misty12024@gmail.com>
2023-09-25 16:56:56 +0900

    변수 연습문제

--- 2/7/index.js
+++ 2/7/index.js
@@ -32,3 +32,25 @@ window.a = 3;
 window.b = 4;
 
 console.log(a + b); //7
+
+// 문제5
+// // setTimeout 5회 반복시키기
+// var i = 5; // 반복문이 다 실행되고 전역변수로 남아있음.
+// var 에서 let으로 바꾸자
+for (let i = 0; i < 5; i++) {
+  // 바로 실행되는 코드가 아님 (1~5회 후에 실행됨)
+  setTimeout(function () {
+    console.log(i);
+  }, i * 1000);
+}
+
+// 문제6. 이벤트리스너 반복시키기
+// let으로 바꾸면 해결
+var 버튼들 = document.querySelectorAll("button");
+var 모달창들 = document.querySelectorAll("div");
+
+for (let i = 0; i < 3; i++) {
+  버튼들[i].addEventListener("click", function () {
+    모달창들[i].style.display = "block";
+  });
+}
