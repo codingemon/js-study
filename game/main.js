eb5861262f569e639632cb99e98e31c5fd569265
snoopy1224 <misty12024@gmail.com>
2023-10-24 12:46:10 +0900

    도라에몽 게임

--- game/main.js
+++ game/main.js
@@ -15,8 +15,15 @@ var dino = {
   draw() {
     ctx.fillStyle = "green";
     ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(img2, this.x, this.y);
   },
 };
+
+// 이미지 넣기
+var img1 = new Image();
+img1.src = "./Img/jingu.png";
+var img2 = new Image();
+img2.src = "./Img/doraemon.png";
 
 // 장애물도 역시 속성부터 object자료에 정리해두면 편리
 // 장애물들은 width, height 이런게 각가 다를 수도 -> 비슷한 Object가 많이 필요
@@ -29,7 +36,8 @@ class Cactus {
   }
   draw() {
     ctx.fillStyle = "red";
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    // ctx.fillRect(this.x, this.y, this.width, this.height); 히트박스
+    ctx.drawImage(img1, this.x, this.y);
   }
 }
 var cactus = new Cactus();
@@ -40,19 +48,62 @@ cactus.draw();
 
 var timer = 0;
 var cactus여러개 = [];
+var 점프timer = 0;
+var animattion;
 
 function 프레임마다다실행할거() {
-  requestAnimationFrame(프레임마다다실행할거);
+  animattion = requestAnimationFrame(프레임마다다실행할거);
   timer++;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  if (timer % 120 === 0) {
+  // 장애물 소환
+  if (timer % 200 === 0) {
     var cactus = new Cactus();
     // 장애물 여러개 관리하기
     cactus여러개.push(cactus);
   }
-  cactus여러개.forEach((a) => {
+  cactus여러개.forEach((a, i, o) => {
+    // x좌표가 0미만이면 제거
+    if (a.x < 0) {
+      o.splice(i, 1);
+    }
+    a.x--;
+
+    충돌하냐(dino, a);
+
     a.draw();
   });
+
+  // 점프기능
+
+  if (점프중 == true) {
+    dino.y--;
+    점프timer++;
+  }
+  if (점프중 == false) {
+    if (dino.y < 200) dino.y++;
+  }
+  if (점프timer > 100) {
+    점프중 = false;
+    점프timer = 0;
+  }
   dino.draw();
 }
 프레임마다다실행할거();
+
+// 충돌확인
+
+function 충돌하냐(dino, cactus) {
+  var x축차이 = cactus.x - (dino.x + dino.width);
+  var y축차이 = cactus.y - (dino.y + dino.height);
+  if (x축차이 < 0 && y축차이 < 0) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    cancelAnimationFrame(animattion);
+  }
+}
+
+var 점프중 = false;
+document.addEventListener("keydown", function (e) {
+  if (e.code === "Space") {
+    점프중 = true;
+  }
+});
